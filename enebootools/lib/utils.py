afb0e98716e19c95a96dcaf298ab0c724bd1b4bc
José A. Fernández <aullasistemas@gmail.com>
2020-06-21 17:10:06 +0200

    avances mergetools

--- enebootools/lib/utils.py
+++ enebootools/lib/utils.py
@@ -11,13 +11,13 @@ def one(iterable, default=None):
 def read_file_list(filepath, filename, errlog = None):
     fullfilename = os.path.join(filepath, filename)
     try:
-        file1 = open(fullfilename, "rb")
+        file1 = open(fullfilename, "r")
     except Exception as e:
         if errlog: errlog("Error al abrir el fichero %s" % fullfilename)
         return []
     txt = [ line.strip() for line in file1 ]
     file1.close()
-    txt2 = [ line for line in txt if len(line) and not line.startswith("#".encode()) ]
+    txt2 = [ line for line in txt if len(line) and not line.startswith("#") ]
     return txt2
     
     
