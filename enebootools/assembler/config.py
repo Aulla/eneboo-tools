4d16e284e77bddd06bd1e58406a12f101f608f27
José A. Fernández <aullasistemas@gmail.com>
2021-06-07 11:18:36 +0200

    fix default config

--- enebootools/assembler/config.py
+++ enebootools/assembler/config.py
@@ -1,13 +1,16 @@
 # encoding: UTF8
-import sys, os.path
+from enebootools import CONF_DIR
+from enebootools.autoconfig.autoconfig import AutoConfigTemplate, ConfigReader
+import sys
+import os.path
+
 
 def filepath(): return os.path.abspath(os.path.dirname(__file__))
-def filedir(x): return os.path.realpath(os.path.join(filepath(),x))
+def filedir(x): return os.path.realpath(os.path.join(filepath(), x))
 
-if __name__ == "__main__": 
-    sys.path.insert(0,filedir("../../"))
-from enebootools.autoconfig.autoconfig import AutoConfigTemplate, ConfigReader
-from enebootools import CONF_DIR
+
+if __name__ == "__main__":
+    sys.path.insert(0, filedir("../../"))
 
 cfg = None
 config_filelist = ['assembler-config.ini']
@@ -21,25 +24,26 @@ class ConfModule(AutoConfigTemplate):
         - ~/git/eneboo-features
     buildcache=string:~/.eneboo-tools/buildcache
     """
-    
+
     def normalize_path(self, path):
         pathlist = path.split("/")
-        if pathlist[0] == "": pathlist[0] = "/"
-        return os.path.join( *[
+        if pathlist[0] == "":
+            pathlist[0] = "/"
+        return os.path.join(*[
             os.path.expanduser(p)
             for p in pathlist
-        ] )
-    
+        ])
+
     def init(self):
-        self.modulefolders = [ self.normalize_path(folder) 
-                            for folder in self.modulefolders]
-    
-        self.featurefolders = [ self.normalize_path(folder) 
-                            for folder in self.featurefolders]
-    
-        self.buildcache = self.normalize_path(self.buildcache) 
-    
-            
+        self.modulefolders = [self.normalize_path(folder)
+                              for folder in self.modulefolders]
+
+        self.featurefolders = [self.normalize_path(folder)
+                               for folder in self.featurefolders]
+
+        self.buildcache = self.normalize_path(self.buildcache)
+
+
 class MergetoolConfig(AutoConfigTemplate):
     """
     patch_qs_rewrite=string:warn
@@ -48,15 +52,11 @@ class MergetoolConfig(AutoConfigTemplate):
     diff_xml_search_move=bool:False
     verbosity_delta=int:0
     """
-    
-    
-    
 
 
-
-def reloadConfig(saveTemplate = False):
-    from . import config as c # --> autoimportación.
-    files = [ os.path.join(CONF_DIR, x) for x in c.config_filelist ]
+def reloadConfig(saveTemplate=False):
+    from . import config as c  # --> autoimportación.
+    files = [os.path.join(CONF_DIR, x) for x in c.config_filelist]
     last_file = files[-1]
     if saveTemplate == "*template*":
         saveTemplate = last_file + ".template"
@@ -66,14 +66,14 @@ def reloadConfig(saveTemplate = False):
     elif not os.path.exists(last_file):
         files = []
         saveTemplate = last_file
-    
-    c.cfg = ConfigReader(files=files, saveConfig = saveTemplate)
-    c.cfg.module = ConfModule(c.cfg,section = "module")
+
+    c.cfg = ConfigReader(files=files, saveConfig=saveTemplate)
+    c.cfg.module = ConfModule(c.cfg, section="module")
     c.cfg.module.init()
-    c.cfg.mergetool = MergetoolConfig(c.cfg,section = "mergetool")
-    
+    c.cfg.mergetool = MergetoolConfig(c.cfg, section="mergetool")
+
     if saveTemplate:
-        f1w = open(saveTemplate, 'wb')
+        f1w = open(saveTemplate, 'w', encoding="UTF-8")
         c.cfg.configini.write(f1w)
         f1w.close()
     return c.cfg
@@ -82,12 +82,14 @@ def reloadConfig(saveTemplate = False):
 def main():
     if len(sys.argv) > 1:
         if sys.argv[1] == 'savetemplate':
-            reloadConfig(saveTemplate = '*template*')
+            reloadConfig(saveTemplate='*template*')
         elif sys.argv[1] == 'update':
-            reloadConfig(saveTemplate = '*update*')
+            reloadConfig(saveTemplate='*update*')
     else:
         reloadConfig()
 
 
-if __name__ == "__main__": main()
-else: reloadConfig()
+if __name__ == "__main__":
+    main()
+else:
+    reloadConfig()
