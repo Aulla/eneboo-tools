9679eb0a91d037e5112b921ddbfc7517338782ce
José A. Fernández <aullasistemas@gmail.com>
2026-01-07 09:02:05 +0100

    repaso funcionalidad copy

--- enebootools/assembler/copylib.py
+++ enebootools/assembler/copylib.py
@@ -15,20 +15,23 @@ def do_copy_action(iface, ext_name, dest_folder):
     features, modules = asmdb.deps(iface, ext_name)
     # Copiamos extensiones
     for feat in features:
-        copy_dep_to_folder(iface, feat, "features" , dest_folder)
+        copy_dep_to_folder(iface, feat, dest_folder)
     for module in modules:
-        copy_dep_to_folder(iface, module, "modules", dest_folder)
+        copy_dep_to_folder(iface, module, dest_folder)
         
         
     
     return True
 
-def copy_dep_to_folder(iface, feat, subfolder, dest_folder):
-    orig_folder = asmdb.dep_main_folder(iface, feat)
-    final_folder = os.path.join(dest_folder, subfolder, feat)
-    #si esxiste finalfolder lo borro
-    print("Copiando %s desde %s a %s" % (feat, orig_folder, final_folder))
-    shutil.copytree(orig_folder, final_folder) 
+def copy_dep_to_folder(iface, feat, dest_folder):
+    orig_folder, subfolder = resolve_main_folder(iface, feat)    
+
+    dest_folder = os.path.join(dest_folder, subfolder, feat)
+    if os.path.exists(dest_folder):
+        shutil.rmtree(dest_folder)
+    
+    print("Copiando %s desde %s a %s" % (feat, orig_folder, dest_folder))
+    shutil.copytree(orig_folder, dest_folder) 
 
 def check_if_folder_is_valid(dest_folder):
     if not os.path.exists(dest_folder):
@@ -38,3 +41,9 @@ def check_if_folder_is_valid(dest_folder):
         print("%s no es un directorio" % dest_folder)
         return False
     return True
+
+def resolve_main_folder(iface, feat):
+    #Extraer ruta previa a la ruta dada
+    orig_folder = asmdb.dep_main_folder(iface, feat)
+    return [orig_folder, str(orig_folder).replace("/%s" % feat, "").split("/")[-1]]
+   
